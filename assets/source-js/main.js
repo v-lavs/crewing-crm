fe557a99c8d6868ef32e99c1543ffdf9a866bb71
v-lavs <lavskavanda@gmail.com>
2018-08-23 22:54:57 +0300

    add responsive swipe cards

--- assets/source-js/main.js
+++ assets/source-js/main.js
@@ -20,11 +20,11 @@ $(document).ready(function () {
       $('.header').removeClass('scroll');
     }
   }
+
   addScrollClass();
   $(window).on('scroll', function () {
     addScrollClass();
   });
-
 
   /**
    * MOB MENU SCRIPT
@@ -47,4 +47,80 @@ $(document).ready(function () {
    */
 
   var lightbox = $('.card-slider a').simpleLightbox();
+
+  /**
+   * RESPONSIVE CARDS SWIPER
+   */
+  function vSlider () {
+    var slider = $('#responsiveCards');
+    var sliderList = slider.find('.col-card-list');
+    var slides = slider.find('.card-slide');
+
+    var slideWidth, slideListWidth, offset;
+
+    function setDimensions () {
+      if($(window).width() <= 991) {
+      slideWidth = slider.width();
+      slideListWidth = slideWidth * slides.length;
+      offset = 0;
+      $('.col-card-list').css({
+        'width': slideListWidth + 'px',
+        'transform': 'translateX(' + '-' + offset + 'px)'
+      });
+      $('.card-slide').css({'width': slideWidth + 'px'});
+    }
+    }
+
+    function resetStyles () {
+      $('.col-card-list').removeAttr('style');
+      $('.card-slide').removeAttr('style');
+    }
+
+    resetStyles();
+    setDimensions();
+
+    function moveLeft () {
+      if (Math.abs(offset) < slideListWidth - slideWidth) {
+        offset += slideWidth;
+        $('.col-card-list').css({
+          'transform': 'translateX(' + '-' + offset + 'px)'
+        });
+      }
+    }
+
+    function moveRight () {
+      if (offset !== 0 && Math.abs(offset) >= slideWidth) {
+        offset -= slideWidth;
+        $('.col-card-list').css({
+          'transform': 'translateX(' + '-' + offset + 'px)'
+        });
+      }
+    }
+
+    function addEvents() {
+      document.getElementById('responsiveCards').addEventListener('swiped-left', moveLeft);
+
+      document.getElementById('responsiveCards').addEventListener('swiped-right',moveRight);
+    }
+
+    function deleteEvents() {
+      document.getElementById('responsiveCards').removeEventListener('swiped-left', moveLeft);
+      document.getElementById('responsiveCards').removeEventListener('swiped-right', moveRight);
+    }
+
+    addEvents();
+
+    $(window).on('resize', function () {
+      if($(window).width() >= 991) {
+        resetStyles();
+        deleteEvents();
+      }
+      else {
+        setDimensions();
+        addEvents();
+      }
+    });
+  }
+
+  vSlider();
 });
