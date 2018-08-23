31de6ba091b65886f3d12f1bfbd5807a1a0ff983
v-lavs <lavskavanda@gmail.com>
2018-08-23 07:42:44 +0300

    add header nav sticky styles

--- assets/source-js/main.js
+++ assets/source-js/main.js
@@ -1,4 +1,28 @@
-jQuery(document).ready(function () {
+/**
+ * CUSTOM SCRIPTS
+ **/
 
+$(document).ready(function () {
+  $('a[href="#"]').on('click', function (e) {
+    e.preventDefault();
+  });
 
+  /**
+   * STICKY-HEADER
+   **/
+  var scrolled;
+
+  function addScrollClass () {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 120) {
+      $('.header').addClass('scroll');
+    } else {
+      $('.header').removeClass('scroll');
+    }
+  }
+
+  addScrollClass();
+  $(window).on('scroll', function () {
+    addScrollClass();
+  });
 });
